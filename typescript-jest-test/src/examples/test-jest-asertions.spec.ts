e09291d43b2f2f42cd41129cef822eca525c1e91
tiagosousa <tiagosousa.tams@hotmail.com>
2024-10-24 14:28:37 +0200

    assertions with JEST

--- typescript-jest-test/src/examples/test-jest-asertions.spec.ts
+++ typescript-jest-test/src/examples/test-jest-asertions.spec.ts
@@ -1,35 +1,29 @@
-describe('Primitive values', () => {
+describe('Primitive Values', () => {
   it('should test jest assertions', () => {
-    const number = 10;
-
-    expect(number).toBeLessThan(11);
-    expect(number).toBeLessThanOrEqual(10);
-
-    expect(number).toBeCloseTo(10.001);
-    expect(number).toBeCloseTo(9.996);
-
-    expect(number).not.toBeNull();
-
-    expect(number).toHaveProperty('toString');
-  });
-
-  it('should split tests', () => {
     const number = 10;
 
     expect(number).toBe(10);
     expect(number).toEqual(10);
-
     expect(number).not.toBeFalsy();
     expect(number).toBeTruthy();
+  });
+  it('should split tests', () => {
+    const number = 10;
 
     expect(number).toBeGreaterThan(9);
     expect(number).toBeGreaterThanOrEqual(10);
+    expect(number).toBeLessThan(11);
+    expect(number).toBeLessThanOrEqual(10);
+
+    // expect(number).toBeCloseTo(9.9901);
+    expect(number).not.toBeNull();
+    expect(number).toHaveProperty('toString');
   });
 });
 
 describe('Objects', () => {
-  it('should test jest assertions with objects', () => {
-    const person = { name: 'Luiz', age: 30 };
+  it('should test jest assertions with Objects', () => {
+    const person = { name: 'Tiago', age: 30 };
     const anotherPerson = { ...person };
 
     expect(person).toEqual(anotherPerson);
@@ -37,6 +31,6 @@ describe('Objects', () => {
     expect(person).not.toHaveProperty('lastName');
     expect(person).toHaveProperty('age', 30);
 
-    expect(person.name).toBe('Luiz');
+    expect(person.name).toBe('Tiago');
   });
 });
--- typescript-jest-test/src/services/persistency.spec.ts
+++ typescript-jest-test/src/services/persistency.spec.ts
@@ -1,25 +1,13 @@
-import { Persistency } from './persistency';
-
-describe('Persistency', () => {
-  afterEach(() => jest.clearAllMocks());
-
-  it('should return undefined', () => {
-    // System under test
-    const sut = new Persistency();
-    expect(sut.saveOrder()).toBeUndefined();
+describe('TESTANDO ALGUMA coisa', () => {
+  it('should return one', () => {
+    const number = 1;
+    expect(number).toBe(1);
   });
+});
 
-  it('should call console.log once', () => {
-    const sut = new Persistency();
-    const consoleSpy = jest.spyOn(console, 'log');
-    sut.saveOrder();
-    expect(consoleSpy).toHaveBeenCalledTimes(1);
-  });
-
-  it('should call console.log with "Pedido salvo com sucesso..."', () => {
-    const sut = new Persistency();
-    const consoleSpy = jest.spyOn(console, 'log');
-    sut.saveOrder();
-    expect(consoleSpy).toHaveBeenCalledWith('Pedido salvo com sucesso...');
+describe('TESTANDO ALGUMA coisa', () => {
+  test('should return Tiago', () => {
+    const nome = 'Tiago';
+    expect(nome).toBe('Tiago');
   });
 });

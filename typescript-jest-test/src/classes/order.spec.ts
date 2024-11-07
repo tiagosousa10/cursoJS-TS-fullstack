3df6fcac45eea09bb9750ba639442de97b404fd5
tiagosousa <tiagosousa.tams@hotmail.com>
2024-11-08 00:12:10 +0100

    testes para todos os metodos de Order

--- typescript-jest-test/src/classes/order.spec.ts
+++ typescript-jest-test/src/classes/order.spec.ts
@@ -1,58 +1,101 @@
-import { CartItem } from "./interfaces/cart-item";
-import { CustomerOrder } from "./interfaces/customer-protocol";
-import { MessagingProtocol } from "./interfaces/messaging-protocol";
-import { PersistencyProtocol } from "./interfaces/persistency-protocol";
-import { ShoppingCartProtocol } from "./interfaces/shopping-cart-protocol";
-import { Order } from "./order";
-import { ShoppingCart } from "./shopping-cart";
+import { CartItem } from './interfaces/cart-item';
+import { CustomerOrder } from './interfaces/customer-protocol';
+import { MessagingProtocol } from './interfaces/messaging-protocol';
+import { PersistencyProtocol } from './interfaces/persistency-protocol';
+import { ShoppingCartProtocol } from './interfaces/shopping-cart-protocol';
+import { Order } from './order';
+import { ShoppingCart } from './shopping-cart';
 
-
-class ShoppingCartMock implements ShoppingCartProtocol{
- get items(): Readonly<CartItem[]>{
-    return[]
+class ShoppingCartMock implements ShoppingCartProtocol {
+  get items(): Readonly<CartItem[]> {
+    return [];
   }
-  addItem(item: CartItem): void{
+  addItem(item: CartItem): void {}
+  removeItem(index: number): void {}
+  total(): number {
+    return 1;
   }
-  removeItem(index: number): void{}
-  total(): number{return 1}
-  totalWithDicount(): number{ return 2}
-  isEmpty(): boolean{ return false}
+  totalWithDicount(): number {
+    return 2;
+  }
+  isEmpty(): boolean {
+    return false;
+  }
   clear(): void {}
 }
 
-class MessagingMock implements MessagingProtocol{
-  sendMessage() {
-  }
+class MessagingMock implements MessagingProtocol {
+  sendMessage() {}
 }
 
-class PersistencyMock implements PersistencyProtocol{
-  saveOrder() {
-  }
+class PersistencyMock implements PersistencyProtocol {
+  saveOrder() {}
 }
 
-class CustomerMock implements CustomerOrder{
+class CustomerMock implements CustomerOrder {
   getName() {
-    return ''
+    return '';
   }
-  getIDN(){
-    return ''
+  getIDN() {
+    return '';
   }
 }
 
-const createSut = ( ) => {
-  const shoppingCartMock = new ShoppingCartMock()
-  const messagingMock = new MessagingMock()
-  const persistencyMock = new PersistencyMock()
-  const customerMock = new CustomerMock()
+const createSut = () => {
+  const shoppingCartMock = new ShoppingCartMock();
+  const messagingMock = new MessagingMock();
+  const persistencyMock = new PersistencyMock();
+  const customerMock = new CustomerMock();
+  const sut = new Order(
+    shoppingCartMock,
+    messagingMock,
+    persistencyMock,
+    customerMock,
+  );
 
-
-
-}
+  return {
+    sut,
+    shoppingCartMock,
+    messagingMock,
+    persistencyMock,
+  };
+};
 
 describe('Order', () => {
-  it('should not checkout if cart is empty'){
-    const order = new Order
-
-
-  }
-})
+  it('should not checkout if cart is empty', () => {
+    const { sut, shoppingCartMock } = createSut();
+    const shoppingCartMockSpy = jest
+      .spyOn(shoppingCartMock, 'isEmpty')
+      .mockReturnValueOnce(true);
+    sut.checkout();
+    expect(shoppingCartMockSpy).toHaveBeenCalledTimes(1);
+    expect(sut.orderStatus).toBe('open');
+  });
+  it('should checkout if cart is not empty', () => {
+    const { sut, shoppingCartMock } = createSut();
+    const shoppingCartMockSpy = jest
+      .spyOn(shoppingCartMock, 'isEmpty')
+      .mockReturnValueOnce(true);
+    sut.checkout();
+    expect(shoppingCartMockSpy).toHaveBeenCalledTimes(1);
+    expect(sut.orderStatus).toBe('open');
+  });
+  it('should send an email to customer', () => {
+    const { sut, messagingMock } = createSut();
+    const messagingMockSpy = jest.spyOn(messagingMock, 'sendMessage');
+    sut.checkout();
+    expect(messagingMockSpy).toHaveBeenCalledTimes(1);
+  });
+  it('should save order', () => {
+    const { sut, persistencyMock } = createSut();
+    const persistencyMockSpy = jest.spyOn(persistencyMock, 'saveOrder');
+    sut.checkout();
+    expect(persistencyMockSpy).toHaveBeenCalledTimes(1);
+  });
+  it('should clear cart', () => {
+    const { sut, shoppingCartMock } = createSut();
+    const shoppingCartMockSpy = jest.spyOn(shoppingCartMock, 'clear');
+    sut.checkout();
+    expect(shoppingCartMockSpy).toHaveBeenCalledTimes(1);
+  });
+});

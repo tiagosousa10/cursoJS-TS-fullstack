ae9730958e14a804326abc0c1e337b2c5e1b718a
tiagosousa <tiagosousa.tams@hotmail.com>
2024-11-13 00:07:25 +0100

    padrao classico e module - singleton

--- design-patterns-typescript/src/creational/singleton/module_a.ts
+++ design-patterns-typescript/src/creational/singleton/module_a.ts
@@ -1,6 +1,6 @@
-import { MyDatabaseClassic } from './db/my-database-classic';
+import { MyDatabaseModule } from './db/my-database-module';
 
-const myDatabaseClassic = MyDatabaseClassic.instance;
+const myDatabaseClassic = MyDatabaseModule;
 myDatabaseClassic.add({ name: 'Tiago', age: 30 });
 myDatabaseClassic.add({ name: 'Joana', age: 21 });
 myDatabaseClassic.add({ name: 'Andre', age: 13 });
--- design-patterns-typescript/src/creational/singleton/module_b.ts
+++ design-patterns-typescript/src/creational/singleton/module_b.ts
@@ -1,7 +1,7 @@
-import { MyDatabaseClassic } from './db/my-database-classic';
+import { MyDatabaseModule } from './db/my-database-module';
 import { myDatabaseClassic as myDatabaseClassicFromModuleA } from './module_a';
 
-const myDatabaseClassic = MyDatabaseClassic.instance;
+const myDatabaseClassic = MyDatabaseModule;
 myDatabaseClassic.add({ name: 'Roberto', age: 36 });
 myDatabaseClassic.add({ name: 'Joao', age: 11 });
 myDatabaseClassic.add({ name: 'Sousa', age: 63 });
